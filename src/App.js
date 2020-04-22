5f83acb9084c1fab9d076ae53bd7e626f5a89505
Julien Guillouzic <35264529+tygrou23@users.noreply.github.com>
2020-04-22 17:55:16 +0200

    Install reactstrap +button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Erreur 404</h1>
-      </header>
-    </div>
+        <div>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>{' '}
+        <Button color="success">Success</Button>{' '}
+        <Button color="warning">Warning</Button>{' '}
+        <Button color="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+        <Button color="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+        <Button color="link">Link</Button>
+      </div>
   );
 }
 
