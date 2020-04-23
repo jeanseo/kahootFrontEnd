3a5e10d18b80ae105e67ea46af457a34e8fb3e52
Julien Guillouzic <35264529+tygrou23@users.noreply.github.com>
2020-04-23 16:40:38 +0200

    Navbar + searchbox + installation outils reactstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
+import Navbars from './Components/atoms/atom';
+import Findquizz from './Components/atoms/search';
+import DashedBox from './Components/atoms/dashedbox';
 
-function App(){
-  return (
-        <div>
-        <Button color="primary">Primary</Button>
-        <Button color="secondary">Secondary</Button>{' '}
-        <Button color="success">Success</Button>{' '}
-        <Button color="warning">Warning</Button>{' '}
-        <Button color="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-        <Button color="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-        <Button color="link">Link</Button>
-      </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div class="body">
+      <Navbars></Navbars>
+      <Findquizz></Findquizz>
+      <DashedBox></DashedBox>
+    </div>
+    );
+  }
 }
 
 export default App;
