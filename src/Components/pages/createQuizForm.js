28fe75f4b30eeea1f2da373544cffe2658cce964
Jeanseo <jsmpub@gmail.com>
2020-04-28 23:45:23 +0200

    affichage formulaire création question

--- src/Components/pages/createQuizForm.js
+++ src/Components/pages/createQuizForm.js
@@ -1,13 +1,25 @@
-import React, { Component } from "react";
+import React, {Component, useState} from "react";
 import {
     Container,
     Row,
-    Col, Form, Card
+    Col, Form, Card, Button
 } from "react-bootstrap";
 
-class editProfile extends Component {
-    render() {
-        return (
+const CreateQuizForm = ({addQuestion}) => {
+    const [question, setQuestion] = useState('');
+    const [timeLimit, setTimeLimit] = useState(20);
+    const [points, setPoints] = useState(1000);
+    const [answer1, setAnswer1] = useState('');
+    const [answer2, setAnswer2] = useState('');
+    const [answer3, setAnswer3] = useState('');
+    const [answer4, setAnswer4] = useState('');
+
+
+    const insertQuestion = () => {
+        console.log(question);
+    };
+
+    return (
             <Container>
                 <Row>
                     <Col>
@@ -15,13 +27,17 @@ class editProfile extends Component {
                         <Form>
                             <Form.Row>
                                 <Col>
-                                    <Form.Control plaintext type="text" placeholder="Ecrivez la question" />
+                                    <Form.Control plaintext type="text"
+                                                  value={question}
+                                                  onChange={e => setQuestion(e.target.value)}
+                                                  placeholder="Ecrivez la question" />
                                 </Col>
                             </Form.Row>
                             <Form.Row>
                                 <Col>
                                     <Form.Label>Limite de temps</Form.Label>
-                                    <Form.Control as="select" value="20">
+                                    <Form.Control as="select" value={timeLimit}
+                                                  onChange={e => setTimeLimit(e.target.value)}>
                                         <option>5</option>
                                         <option>10</option>
                                         <option>20</option>
@@ -34,7 +50,9 @@ class editProfile extends Component {
                                 </Col>
                                 <Col>
                                     <Form.Label>Points</Form.Label>
-                                    <Form.Control as="select" value="20">
+                                    <Form.Control as="select"
+                                                  value={points}
+                                                  onChange={e => setPoints(e.target.value)}>
                                         <option>0</option>
                                         <option>1000</option>
                                         <option>2000</option>
@@ -46,7 +64,10 @@ class editProfile extends Component {
                                     <Card>
                                         <Card.Header as="h5"></Card.Header>
                                         <Card.Title>
-                                            <Form.Control plaintext type="text" placeholder="Réponse 1" />
+                                            <Form.Control plaintext type="text" placeholder="Réponse 1"
+                                                          value={answer1}
+                                                          onChange={e => setAnswer1(e.target.value)}
+                                            />
                                         </Card.Title>
                                     </Card>
                                 </Col>
@@ -54,7 +75,10 @@ class editProfile extends Component {
                                     <Card>
                                         <Card.Header as="h5"></Card.Header>
                                         <Card.Title>
-                                            <Form.Control plaintext type="text" placeholder="Réponse 2" />
+                                            <Form.Control plaintext type="text" placeholder="Réponse 2"
+                                                          value={answer2}
+                                                          onChange={e => setAnswer2(e.target.value)}
+                                            />
                                         </Card.Title>
                                     </Card>
                                 </Col>
@@ -64,7 +88,10 @@ class editProfile extends Component {
                                     <Card>
                                         <Card.Header as="h5"></Card.Header>
                                         <Card.Title>
-                                            <Form.Control plaintext type="text" placeholder="Réponse 3" />
+                                            <Form.Control plaintext type="text" placeholder="Réponse 3"
+                                                          value={answer3}
+                                                          onChange={e => setAnswer3(e.target.value)}
+                                            />
                                         </Card.Title>
                                     </Card>
                                 </Col>
@@ -72,18 +99,24 @@ class editProfile extends Component {
                                     <Card>
                                         <Card.Header as="h5"></Card.Header>
                                         <Card.Title>
-                                            <Form.Control plaintext type="text" placeholder="Réponse 4" />
+                                            <Form.Control plaintext type="text" placeholder="Réponse 4"
+                                                          value={answer4}
+                                                          onChange={e => setAnswer4(e.target.value)}
+                                            />
                                         </Card.Title>
                                     </Card>
                                 </Col>
                             </Form.Row>
+
                         </Form>
+                        <Button variant="primary" onClick={insertQuestion}>
+                            Submit
+                        </Button>
                     </Col>
                 </Row>
 
             </Container>
         );
-    }
-}
+};
 
-export default editProfile;
+export default CreateQuizForm;
